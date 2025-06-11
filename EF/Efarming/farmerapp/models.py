176545ebfd56f4f602519536d723c557d9acecfa
Vaishnavi Mohite <mohitevaishnavi067@gmail.com>
2025-06-11 06:46:37 +0530

    Initial commit with EF as normal folder

---
+++ EF/Efarming/farmerapp/models.py
@@ -0,0 +1,90 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+#from Efarming import farmerapp
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=30, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
+    image = models.FileField(null=True)
+    name = models.CharField(max_length=30, null=True)
+    price = models.IntegerField(null=True)
+    desc = models.TextField(null=True)
+
+    def __str__(self):
+        return self.category.name+"--"+self.name
+
+class Status(models.Model):
+    name = models.CharField(max_length=20, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    dob = models.DateField(null=True)
+    city = models.CharField(max_length=30, null=True)
+    address = models.CharField(max_length=50, null=True)
+    contact = models.CharField(max_length=10, null=True)
+    user_type = models.CharField(max_length=10, null=True)
+    status =  models.CharField(max_length=30, null=True, default="Pending")
+    image = models.FileField(null=True)
+
+    def __str__(self):
+        return self.user.username
+
+
+class Cart(models.Model):
+    profile = models.ForeignKey(Profile,on_delete=models.CASCADE,null=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True)
+    def __str__(self):
+        return self.profile.user.username + " . " + self.product.name
+
+
+from django.contrib.auth.models import User
+
+
+
+
+class Booking(models.Model):
+    status = models.ForeignKey(Status, on_delete=models.CASCADE, null=True)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True)  # customer/buyer
+    farmer = models.ForeignKey(Profile, related_name='farmer_bookings', on_delete=models.CASCADE, null=True)  # farmer
+
+    booking_id = models.CharField(max_length=200, null=True)
+    quantity = models.CharField(max_length=100, null=True)
+    payment_id = models.CharField(max_length=200, null=True)
+    payment_status = models.CharField(max_length=200, null=True, default="Pending")
+    book_date = models.CharField(max_length=30, null=True)
+    total = models.IntegerField(null=True)
+
+    def __str__(self):
+        return self.book_date + " " + self.profile.user.username
+
+
+
+class BookingProduct(models.Model):
+    booking = models.ForeignKey(Booking, on_delete=models.CASCADE, related_name='booking_products')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.product.name} x {self.quantity}"
+
+
+
+class Send_Feedback(models.Model):
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True)
+    message1 = models.TextField(null=True)
+    date = models.CharField(max_length=30, null=True)
+
+    def __str__(self):
+        return self.profile.user.username
+
+
